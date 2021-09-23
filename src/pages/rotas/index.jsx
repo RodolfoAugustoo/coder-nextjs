74176de21538dc9818ca603a86e84263c7ce0b9d
Rodolfo Augusto <r.augusto1892@gmail.com>
2021-09-23 11:43:41 -0300

    Navegação entre páginas.

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,8 +1,28 @@
-export default function Rota(){
+import Link from "next/link"
+
+export default function Rota() {
 
   return (
     <div>
       <h1>Rota</h1>
+
+      <ul>
+        <li>
+          <Link href='/params/codigo=123&nome=Rodolfo'>
+            Codigo e nome
+          </Link>
+        </li>
+        <li>
+          <Link href='/123/codigo'>
+            Codigo
+          </Link>
+        </li>
+        <li>
+          <Link href='/xyz/dinamica2'>
+            Dinâmica 2
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
---
+++ src/pages/rotas/params.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router'
+
+export default function Params(){
+
+  const router = useRouter()
+
+  const id = router.query.id
+  const nome = router.query.nome
+
+  return(
+    <div>
+      <h1>
+        Rotas Params: {id} e {nome}
+      </h1>
+    </div>
+  )
+}
