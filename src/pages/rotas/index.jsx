8ab811bb404c7e1ef9ccee0c3ac278f985d0c4b9
Rodolfo Souza <rodolfo.souza@cadmus.com.br>
2021-09-29 09:16:18 -0300

    NAvegação com useRouter.

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,28 +1,51 @@
-import Link from "next/link"
+import {useRouter} from 'next/router'
+import Link from "next/link";
 
 export default function Rota() {
+  
+  const router = useRouter()
 
+  function navegaComParams(path, queryParams){
+    router.push({
+      pathname:'/rotas/params',
+      query:{
+        id: 123,
+        nome: 'Rodolfo'
+      }
+    })
+  }
+
+  function urlDireta(url){
+    router.push(url)
+  }
+  
   return (
     <div>
       <h1>Rota</h1>
-
       <ul>
         <li>
-          <Link href='/params/codigo=123&nome=Rodolfo'>
-            Codigo e nome
-          </Link>
+          <Link href="/rotas/params?id=123&nome=Rodolfo">Codigo e nome</Link>
         </li>
         <li>
-          <Link href='/123/codigo'>
-            Codigo
-          </Link>
+          <Link href="/123/codigo">Codigo</Link>
         </li>
         <li>
-          <Link href='/xyz/dinamica2'>
-            Dinâmica 2
-          </Link>
+          <Link href="/xyz/dinamica2">Dinâmica 2</Link>
+        </li>
+      </ul>
+
+      <h2>useRouter</h2>
+      <ul>
+        <li onClick={() => navegaComParams()}>
+          <span>Params</span>
+        </li>
+        <li onClick={() => urlDireta("/123/codigo")}>
+          <span>Codigo</span>
+        </li>
+        <li onClick={() => router.push("/xyz/dinamica2")}>
+          <span>Dinâmica 2</span>
         </li>
       </ul>
     </div>
-  )
+  );
 }
